a6c560798ba1be875914c9073a1cd9cdbdec27f4
Jhonatan <jhonatanjonh@outlook.com>
2021-09-27 11:23:04 -0300

    Incluindo looping para puxar dados do excel

--- home.py
+++ home.py
@@ -3,8 +3,6 @@ import time
 import pandas as pd
 from IPython.display import display
 
-tabela = pd.read_excel("requisicoes.xlsx")
-
 navegator = webdriver.Chrome()
 
 navegator.get("http://erpb.orsa.mx:31470/psp/cpfb91pr/EMPLOYEE/ERP/?&cmd=login&languageCd=POR&")
@@ -13,8 +11,12 @@ navegator.find_element_by_xpath('//*[@id="userid"]').send_keys('JSANTOS')
 
 navegator.find_element_by_xpath('//*[@id="pwd"]').send_keys('987654321%')
 
-for i, bu in enumerate(tabela['BU']):
-    if(tabela.loc[i, "ID_Requisição"] != ""):
+navegator.find_element_by_xpath('//*[@id="login"]/div/div[1]/div[8]/input').click()
+
+tabela = pd.read_excel("requisicoes.xlsx")
+
+for i, buu in enumerate(tabela['BU']):
+    if(tabela.loc[i, "ID_requisicao"] == ""):
         continue
     print(tabela.loc[i, "Titulo"])
     BU = tabela.loc[i, "BU"]
@@ -27,7 +29,7 @@ for i, bu in enumerate(tabela['BU']):
     valor = tabela.loc[i, "Valor"]
     id_requisicao = tabela.loc[i, "ID_requisicao"]
 
-    navegator.find_element_by_xpath('//*[@id="login"]/div/div[1]/div[8]/input').click()
+    time.sleep(3)
 
     navegator.get('https://erpb.orsa.mx:31475/psp/cpfb91pr/EMPLOYEE/ERP/c/REQUISITION_ITEMS.REQUISITIONS.GBL?FolderPath=PORTAL_ROOT_OBJECT.EPPO_PURCHASING.EPCO_REQUISITIONS.EP_REQUISITIONS_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder')
 
@@ -35,11 +37,11 @@ for i, bu in enumerate(tabela['BU']):
 
     navegator.find_element_by_xpath('//*[@id="REQ_ADD_SRCH_BUSINESS_UNIT"]').clear()
 
-    navegator.find_element_by_xpath('//*[@id="REQ_ADD_SRCH_BUSINESS_UNIT"]').send_keys(BU)
+    navegator.find_element_by_xpath('//*[@id="REQ_ADD_SRCH_BUSINESS_UNIT"]').send_keys(str(BU))
 
     navegator.find_element_by_xpath('//*[@id="#ICSearch"]').click()
 
-    #navegator.switch_to.frame('TargetContent')
+#navegator.switch_to.frame('TargetContent')
     time.sleep(5)
 
     navegator.find_element_by_xpath('//*[@id="REQ_HDR_REQ_NAME"]').send_keys(titulo)
@@ -50,21 +52,21 @@ for i, bu in enumerate(tabela['BU']):
 
     time.sleep(5)
 
-    navegator.find_element_by_xpath('//*[@id="REQ_LINE_WRK_INV_ITEM_ID$0"]').send_keys(item) #preencher com item
+    navegator.find_element_by_xpath('//*[@id="REQ_LINE_WRK_INV_ITEM_ID$0"]').send_keys(str(item)) #preencher com item
 
     time.sleep(4)
 
-    navegator.find_element_by_xpath('//*[@id="REQ_LINE_WRK_INV_ITEM_ID$0"]').send_keys(item) #preencher com item
+    navegator.find_element_by_xpath('//*[@id="REQ_LINE_WRK_INV_ITEM_ID$0"]').send_keys(str(item)) #preencher com item
 
     navegator.find_element_by_xpath('//*[@id="REQ_LINE_QTY_REQ$0"]').clear() #preencher quantidade
 
-    navegator.find_element_by_xpath('//*[@id="REQ_LINE_QTY_REQ$0"]').send_keys(quantidade) #preencher quantidade
+    navegator.find_element_by_xpath('//*[@id="REQ_LINE_QTY_REQ$0"]').send_keys(len(str(quantidade)))#preencher quantidade
 
     time.sleep(3)
 
     navegator.find_element_by_xpath('//*[@id="REQ_LINE_QTY_REQ$0"]').clear() #preencher quantidade
 
-    navegator.find_element_by_xpath('//*[@id="REQ_LINE_QTY_REQ$0"]').send_keys(quantidade) #preencher quantidade
+    navegator.find_element_by_xpath('//*[@id="REQ_LINE_QTY_REQ$0"]').send_keys(len(str(quantidade))) #preencher quantidade
 
     time.sleep(4)
 
@@ -78,15 +80,17 @@ for i, bu in enumerate(tabela['BU']):
 
     navegator.switch_to.default_content()
 
-    time.sleep(3)
-
     navegator.find_element_by_xpath('//*[@id="#ICOK"]').click()
+
+    time.sleep(3)
 
     navegator.switch_to.frame('TargetContent')
 
+    time.sleep(1)
+
     navegator.find_element_by_xpath('//*[@id="REQ_LINE_SHIP_SHIPTO_ID$0"]').clear() # Entrega
 
-    navegator.find_element_by_xpath('//*[@id="REQ_LINE_SHIP_SHIPTO_ID$0"]').send_keys(budge_unidade) # Entrega
+    navegator.find_element_by_xpath('//*[@id="REQ_LINE_SHIP_SHIPTO_ID$0"]').send_keys(str(budge_unidade)) # Entrega
 
     time.sleep(3)
 
@@ -98,9 +102,9 @@ for i, bu in enumerate(tabela['BU']):
 
     time.sleep(3)
 
-    navegator.find_element_by_xpath('//*[@id="#ICYes"]').click() # confirmar e seguir
+    navegator.find_element_by_xpath('//*[@id="#ICYes"]').click()# confirmar e seguir
 
-    time.sleep(10)
+    time.sleep(2)
 
     navegator.find_element_by_xpath('//*[@id="#ICOK"]').click() #confirmar e seguir
     navegator.find_element_by_xpath('//*[@id="#ICOK"]').click() #confirmar e seguir
@@ -113,7 +117,7 @@ for i, bu in enumerate(tabela['BU']):
 
     navegator.find_element_by_xpath('//*[@id="OPERATING_UNIT$0"]').clear()
 
-    navegator.find_element_by_xpath('//*[@id="OPERATING_UNIT$0"]').send_keys(budge_unidade) #preenchendo unidade de distribuição orçamentaria
+    navegator.find_element_by_xpath('//*[@id="OPERATING_UNIT$0"]').send_keys(str(budge_unidade)) #preenchendo unidade de distribuição orçamentaria
 
     #navegator.switch_to.frame('ptModFrame_5')
 
@@ -133,10 +137,7 @@ for i, bu in enumerate(tabela['BU']):
 
     navegator.find_element_by_xpath('//*[@id="REQ_LINE_WRK_PRICE_REQ_C$0"]').clear()
 
-    navegator.find_element_by_xpath('//*[@id="REQ_LINE_WRK_PRICE_REQ_C$0"]').send_keys(valor)
+    navegator.find_element_by_xpath('//*[@id="REQ_LINE_WRK_PRICE_REQ_C$0"]').send_keys(str(valor))
 
     tabela.loc[i, "ID_requisicao"] = "feito"
-
-
-
 
